350d2044bf6bd38a01708112d82f8d2c394b259e
lorence <lorence@enclave.vn>
2018-01-29 15:22:26 +0700

    Done activity

--- app/src/main/java/io/branch/google_analyticsexample/SecondActivity.java
+++ app/src/main/java/io/branch/google_analyticsexample/SecondActivity.java
@@ -3,11 +3,14 @@ package io.branch.google_analyticsexample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import io.branch.google_analyticsexample.app.Application;
+
 public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        Application.getInstance().trackScreenView("Second Screen");
     }
 }
