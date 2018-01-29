22fe978315a163cdcb3e7546bbc7ab3f0736cb1f
lorence <lorence@enclave.vn>
2018-01-29 17:23:41 +0700

    Explore modules exception

--- app/src/main/java/io/branch/google_analyticsexample/HomeActivity.java
+++ app/src/main/java/io/branch/google_analyticsexample/HomeActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -69,6 +70,33 @@ public class HomeActivity extends AppCompatActivity {
                 Application.tracker().send(eventBuilder.build());
 
                 Toast.makeText(getApplicationContext(), "Event \'Book\' \'Download\' \'Event example\' is sent. Check it on Google Analytics Dashboard!", Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+        /**
+         * Tracking Exception Manually
+         * All known exceptions can be tracking this way
+         * using Try & Catch
+         */
+        btnException.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    String name = null;
+                    if (name.equals("ravi")) {
+                    /* Never comes here as it throws null pointer exception */
+                    }
+                } catch (Exception e) {
+                    // Tracking exception
+                    // Application.getInstance().trackException(e);
+                    Application.tracker().send(new HitBuilders.ExceptionBuilder()
+                            .setDescription(e.toString())
+                            .setFatal(true)
+                            .build());
+                    Toast.makeText(getApplicationContext(), getString(R.string.toast_track_exception), Toast.LENGTH_LONG).show();
+                    Log.e(TAG, "Exception: " + e.getMessage());
+                }
             }
         });
     }
